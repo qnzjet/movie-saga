c14a1d5f1a77f02e30ef75170395b81e68686c04
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-06-21 18:16:26 -0500

    movies route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
+        <Header />
         <p>Empty Page</p>
       </div>
     );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  // Renders the entire app on the DOM
+  render() {
+    return (
+      <div>
+        <h1>Nagle Movie Sagas Weekend Challenge</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
